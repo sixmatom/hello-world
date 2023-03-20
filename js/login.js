2991e1b2d077c3088375a8bca1dea09a9a8e9c77
Tom Sixma <sixmatom@gmail.com>
2023-03-20 14:25:17 +0100

    added getrooms();

--- js/login.js
+++ js/login.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
                 password: password,
                 }),
             success: function (data) {
+                getRooms();
                 var token = data.token;
                 
 
