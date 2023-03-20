833eeb02dfe07aba413fb053fcd92eb523f99dc2
Tom Sixma <55391087+sixmatom@users.noreply.github.com>
2023-03-20 11:48:17 +0100

    Add files via upload

---
+++ js/booking.js
@@ -0,0 +1,133 @@
+localStorage.getItem("jwtToken")
+function getRooms (name){
+    
+  $.ajax({
+  url: BASE_URL + "/getRooms",
+  type: "POST",
+  data: {
+    name:name
+  },
+  beforeSend: function (xhr) {
+    xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("jwtToken"));
+  },
+  success: function (data) {
+    // Do something with the booking data, e.g. create calendar events
+    const rooms = data
+    localStorage.setItem("Rooms",JSON.stringify(rooms));
+  },
+  error: function (jqXHR, textStatus, errorThrown) {
+    console.error('Error:', textStatus, errorThrown);
+  }
+});
+}
+
+  
+function getBooking (){
+    
+    $.ajax({
+    url: BASE_URL + "/bookings",
+    type: "GET",
+    dataType: 'text',
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("jwtToken"));
+    },
+    success: function (data) {
+      // Do something with the booking data, e.g. create calendar events
+      localStorage.setItem("Bookings", data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error('Error:', textStatus, errorThrown);
+    }
+  });
+}
+
+function getBookingByRoom (roomName){
+  
+  
+  $.ajax({
+  url: BASE_URL + "/getBookingByRoom",
+  type: "POST",
+  data: {
+    roomName:roomName
+  },
+  beforeSend: function (xhr) {
+    xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("jwtToken"));
+  },
+
+  success: function (data) {
+    localStorage.setItem(roomName,JSON.stringify(data));
+    
+    // Do something with the booking data, e.g. create calendar events
+   
+  },
+  error: function (jqXHR, textStatus, errorThrown) {
+    console.error('Error:', textStatus, errorThrown);
+  }
+});
+}
+
+function makeBooking(name, timeStart, timeEnd, email, calendar) {
+  checkTokenExpiration(localStorage.getItem("jwtToken"))
+  $.ajax({
+    url: BASE_URL + "/makeBooking",
+    type: "POST",
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("jwtToken"));
+    },
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    
+    data: JSON.stringify({
+      "room": {
+        "name": name
+      },
+      "timeStart": timeStart,
+      "timeEnd": timeEnd,
+      "userEmail":email,
+      "user": {
+        "email": email
+      }
+    }),
+    success:function (data) {
+      getBookingByRoom(name);
+    }, 
+    
+    error: function (xhr, status, error) {
+      console.log(xhr.responseText);
+  } 
+  });
+}
+function deleteBooking(bookingToken, roomName, userEmail) {
+  checkTokenExpiration(localStorage.getItem("jwtToken"))
+  $.ajax({
+  url: BASE_URL + "/cancelBooking",
+  type: "POST",
+  beforeSend: function(xhr) {
+    xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem("jwtToken"));
+  },
+   
+  data: {
+    email: userEmail,
+    token : bookingToken
+  },
+  
+      
+  success:function (data) {
+        
+    getBookingByRoom(roomName)
+  
+},
+  
+  error: function (xhr, status, error) {
+    console.log(xhr.responseText);
+    
+  }
+  
+});
+}
+
+
+
+  
+  
---
+++ js/login.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#login-form").submit(function (e) {
+        e.preventDefault();
+
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        $.ajax({
+            url: BASE_URL + "/test/authenticate",
+            type: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            data: JSON.stringify({
+                email: email,
+                password: password,
+                }),
+            success: function (data) {
+                var token = data.token;
+                
+
+                // Store the token in local storage
+                localStorage.setItem("jwtToken", token);
+                window.location.href = "../static/home.html";
+            },
+            error: function (xhr, status, error) {
+                const errorMessage = "Error: " + xhr.responseJSON.error;
+                const errorElement = document.querySelector(".w-form-fail > div");
+                errorElement.textContent = errorMessage;
+
+                
+                console.log("Error: " + xhr.responseJSON.error );
+            }
+        });
+    });
+});
