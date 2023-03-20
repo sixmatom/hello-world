d1d5be0df6518b2e467e025482b1ec5f8eab0ee1
Tom Sixma <sixmatom@gmail.com>
2023-03-20 14:53:28 +0100

    getrooms

--- js/login.js
+++ js/login.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
 
                 // Store the token in local storage
                 localStorage.setItem("jwtToken", token);
+                getRooms("test1")
                 window.location.href = "../home.html";
             },
             error: function (xhr, status, error) {
--- js/register.js
+++ js/register.js
@@ -27,6 +27,7 @@ console.log(firstName,lastName)
 
                 // Store the token in local storage
                 localStorage.setItem("jwtToken", token);
+                getRooms("test1")
                 window.location.href = "../home.html";
             },
             error: function (xhr, status, error) {
