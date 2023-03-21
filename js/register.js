d51033c8734d0a38778334b092f468623c125220
Tom Sixma <sixmatom@gmail.com>
2023-03-21 14:57:27 +0100

    changed register.js

--- js/register.js
+++ js/register.js
@@ -27,8 +27,9 @@ console.log(firstName,lastName)
 
                 // Store the token in local storage
                 localStorage.setItem("jwtToken", token);
-                getRooms("test1")
-                // window.location.href = "../home.html";
+                getRooms("test1").then(function(){
+                window.location.href = "../home.html";
+                });
             },
             error: function (xhr, status, error) {
                 const errorMessage = "Error: " + xhr.responseJSON.error;
